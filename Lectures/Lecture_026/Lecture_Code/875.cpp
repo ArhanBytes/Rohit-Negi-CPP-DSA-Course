47339256baabd882d71c4d98ab96c11c6e68a2d4
Arhan <muhammadarhan7t6@gmail.com>
2024-04-09 23:59:49 +0500

    Lecture 26: Binary Search Problems (Part 4)

---
+++ Lectures/Lecture_026/Lecture_Code/875.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+    int minEatingSpeed(vector<int> &piles, int h)
+    {
+        int start = 0, end = 0, mid, ans, n = piles.size();
+        long long sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += piles[i];
+            end = max(end, piles[i]);
+        }
+        start = sum / h;
+
+        if (!start)
+            start = 1;
+
+        while (start <= end)
+        {
+            mid = start + (end - start) / 2;
+            int total_time = 0;
+            for (int i = 0; i < n; i++)
+            {
+                total_time += piles[i] / mid;
+                if (piles[i] % mid)
+                    total_time++;
+            }
+
+            if (total_time > h)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                ans = mid;
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
---
+++ Lectures/Lecture_026/README.md
@@ -0,0 +1,6 @@
+# Binary Search Problems (Part 3)
+
+| Problems  | Solution     | Source |
+| :-------- | :-------: | :--------: |
+| [Allocate minimum number of pages](https://practice.geeksforgeeks.org/problems/aggressive-cows/0) | [Solution](https://github.com/ArhanBytes/Rohit-Negi-CPP-DSA-Course/blob/main/Lectures/Lecture_026/Lecture_Code/agressiveCow.cpp)| GFG |
+| [875. Koko Eating Bananas](https://leetcode.com/problems/koko-eating-bananas/description/) | [Solution](https://github.com/ArhanBytes/Rohit-Negi-CPP-DSA-Course/blob/main/Lectures/Lecture_026/Lecture_Code/agressiveCow.875)| GFG |
