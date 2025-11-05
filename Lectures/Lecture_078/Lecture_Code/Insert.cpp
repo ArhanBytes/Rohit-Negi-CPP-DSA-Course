bba298f1bb1ff723cbeb90b0dd84a41a2687e69d
Arhan <muhammadarhan7t6@gmail.com>
2025-11-05 12:29:56 +0500

    Lecture 78: Introduction To Linked List

---
+++ Lectures/Lecture_078/Lecture_Code/Insert.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+class LinkedList
+{
+    Node *Head;
+
+public:
+    LinkedList()
+    {
+        Head = NULL;
+    }
+
+    void insert_at_start(int data)
+    {
+        if (Head == NULL)
+            Head = new Node(data);
+        else
+        {
+            Node *temp = new Node(data);
+            temp->next = Head;
+            Head = temp;
+        }
+    }
+
+    // insert at start using recursion
+    Node *insert_at_start(int *arr, int index, int size, Node *next)
+    {
+        // base case
+        if (index == size)
+            return next;
+
+        Node *temp = new Node(arr[index]);
+        temp->next = next;
+
+        return insert_at_start(arr, index + 1, size, temp);
+    }
+
+    void insert_at_start_using_recurison(int *arr, int size)
+    {
+        Head = insert_at_start(arr, 0, size, Head);
+    }
+
+    // insert at last
+    void insert_at_last(int data)
+    {
+        if (Head == NULL)
+            Head = new Node(data);
+        else
+        {
+            Node *temp = Head;
+            while (temp->next != NULL)
+                temp = temp->next;
+
+            temp->next = new Node(data);
+        }
+    }
+
+    // Insert at last using recursion
+    Node *insert_at_last(int *arr, int index, int size)
+    {
+        // base case
+        if (index == size)
+            return NULL;
+
+        Node *temp = new Node(arr[index]);
+        temp->next = insert_at_last(arr, index + 1, size);
+
+        return temp;
+    }
+
+    void insert_at_last_using_recursion(int *arr, int size)
+    {
+        if (Head == NULL)
+            Head = insert_at_last(arr, 0, size);
+        else
+        {
+            Node *temp = Head;
+            while (temp->next != NULL)
+                temp = temp->next;
+
+            temp->next = insert_at_last(arr, 0, size);
+        }
+    }
+
+    // insert at particular location
+    void insert_at_particular(int position, int data)
+    {
+        if (position < 0)
+        {
+            cout << "Invalid Position :)" << endl;
+        }
+        else if (position == 0)
+            insert_at_start(data);
+        else
+        {
+            Node *traverse = Head;
+            while (--position && traverse != NULL)
+                traverse = traverse->next;
+
+            if (traverse == NULL)
+            {
+                cout << "Invalid Position :)" << endl;
+                return;
+            }
+
+            Node *temp = new Node(data);
+            temp->next = traverse->next;
+            traverse->next = temp;
+        }
+    }
+
+    // print linkedlist
+    void print_linkedlist()
+    {
+        Node *temp = Head;
+        cout << "Printing LinkedList: ";
+        while (temp != NULL)
+        {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main()
+{
+    LinkedList L1;
+
+    int arr[] = {2, 4, 6, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // 1: insert at start
+    for (int i = 0; i < n; i++)
+    {
+        L1.insert_at_start(arr[i]);
+    }
+
+    // 2: insert at last
+    L1.insert_at_last(9);
+    L1.insert_at_last(11);
+
+    // 3: insert at end using recursion
+    int arr2[] = {10, 20, 30, 40};
+    int n2 = sizeof(arr) / sizeof(arr[0]);
+
+    L1.insert_at_last_using_recursion(arr2, n2);
+
+    // 4: insert at start using recursion
+    int arr3[] = {100, 200, 300, 400};
+    int n3 = sizeof(arr) / sizeof(arr[0]);
+    L1.insert_at_start_using_recurison(arr3, n3);
+
+    // 5: insert at particular location
+    L1.insert_at_particular(14, 3);
+    // printing linkedlist
+    L1.print_linkedlist();
+
+    return 0;
+}
---
+++ Lectures/Lecture_078/README.md
@@ -0,0 +1,3 @@
+# Link List in C++
+
+- [More about Linked List in C++](https://www.geeksforgeeks.org/cpp/cpp-linked-list/)
