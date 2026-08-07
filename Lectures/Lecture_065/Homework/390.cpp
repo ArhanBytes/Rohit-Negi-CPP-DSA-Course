13aa5e320f35ba684da2cfc5ba6e3737b0163f0f
Arhan <muhammadarhan7t6@gmail.com>
2026-08-07 14:43:44 +0500

    Lecture 65: Permutation with repetition || Ways to Sum N

--- Lectures/Lecture_065/Homework/390.cpp
+++ Lectures/Lecture_065/Homework/390.cpp
@@ -1,1 +1,24 @@
-// pending
+class Solution {
+public:
+    int LR(int &n, int &head, int &step, bool &left)
+    {
+        // base case
+        if (n == 1) return head;
+
+        // head changes if start from left or we have odd elements in array
+        if(left || n & 1) 
+            head = head + step;
+
+        n /= 2;
+        step *= 2;
+        left = !left;
+
+        return LR(n, head, step, left);
+    }
+
+    int lastRemaining(int n) {
+        int head = 1, step = 1;
+        bool left = true;
+        return LR(n, head, step, left);
+    }
+};
